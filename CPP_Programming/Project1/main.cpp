2a8b958a431250f0f4eca9570904fb88de1e0613
Felipe <felipe.lopez@fanuc.eu>
2020-11-04 22:47:48 +0100

    using namespace std

--- CPP_Programming/Project1/main.cpp
+++ CPP_Programming/Project1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+using namespace std;
 
 int main () {
-    std::cout <<"Hola Felipe López" <<std::endl;
-    std::cout <<"Buen día" <<std::endl;
+    std::cout <<"Hola Felipe Lopez" <<std::endl;
+    std::cout <<"Buen dia" <<std::endl;
+    std::cout <<"bye" <<std::endl;
     return 0;
 }
