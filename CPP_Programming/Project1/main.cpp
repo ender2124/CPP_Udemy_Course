5c9f17405bc389208085a7f06a776e3cf7ddb2da
Felipe <felipe.lopez@fanuc.eu>
2020-11-05 00:44:23 +0100

    inicializacion de variables

--- CPP_Programming/Project1/main.cpp
+++ CPP_Programming/Project1/main.cpp
@@ -1,9 +1,31 @@
+////https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535336#questions
+
 #include <iostream>
 using namespace std;
 
 int main () {
-    std::cout <<"Hola Felipe Lopez" <<std::endl;
-    std::cout <<"Buen dia" <<std::endl;
-    std::cout <<"bye" <<std::endl;
+    
+    //Variables definition
+    int num1;
+    int num2;
+    double num3;        
+    string palabra;
+    
+    
+    cout <<"Hola Felipe Lopez" <<endl;
+    cout << "enter a first integer: ";
+    cin>> num1;
+    
+    cout << "enter a second integer: ";
+    cin >>num2;
+    
+    cout << "you entered " <<num1 << " and " <<num2<< endl;
+    
+    //Escribiendo un string 
+    
+    cout <<"write a palabra "<< endl;
+    cin >> palabra;
+    cout << "you wrote " <<palabra << endl;
     return 0;
 }
+ 
--- CPP_Programming/Project2/main.cpp
+++ CPP_Programming/Project2/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+//Section 6: Variables and Constants
+//https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535354#questions
 
+#include <iostream>
+using namespace std;
 int main () {
-    std::cout <<"My template" <<std::endl;
     
+    int width;
+    int length;
+    cout << "enter the width of the room" <<endl;
+    cin >> width;
+    
+    cout << "enter the length of the room" <<endl;
+    cin >> length;
+    
+    cout << "area equal to: " << width * length << endl;
     return 0;
 }
